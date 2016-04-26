fae075839d003fa2a160fbb49d1370a47b7fbc6b
Sarah Hicks <sarah.hicks@emerson.com>
2016-04-26 08:26:16 -0500

    Shift to reddit base

--- app.ts
+++ app.ts
@@ -7,21 +7,27 @@ import { Component } from "angular2/core";
 
 // Component annotation: at min, defines tag and what to display
 @Component({
-    selector: 'hello-world',
+    selector: 'reddit',
     template: `
-    <ul>
-        <li *ngFor="#name of names">Hello {{ name }}</li>
-    </ul>
+    <form class="ui large form segment">
+        <h3 class="ui header">Add a Link</h3>
+        
+        <div class="field">
+            <label for="title">Title:</label>
+            <input name="title">
+        </div>
+        <div class="field">
+            <label for="link">Link:</label>
+            <input name="link">
+        </div>
+    </form>
     `
 })
     // Component definition class with property/type and value
-class HelloWorld {
-    names: string[];
-
+class RedditApp {
     constructor() {
-        this.names = ['Sarah', 'Robert', 'John'];
     }
 }
 
 // boots the app and first argument is the component
-bootstrap(HelloWorld);
+bootstrap(RedditApp);
